7ceee51e585a08d46e16abff155b2702f78b5109
Dmitriy Prokushev <martcarrefour@github.com>
2018-04-10 00:32:58 +0300

    useref tests

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,5 +1,5 @@
 module.exports = function (){
-    $.gulp.task('pug', function () {
+    $.gulp.task('pug:dev', function () {
         return $.gulp.src('src/pug/pages/*.pug')
             .pipe($.gp.pug({
                 pretty:true
@@ -7,6 +7,15 @@ module.exports = function (){
             .pipe($.gp.wiredep({
                 directory: './bower_components'
             }))
+            .pipe($.gulp.dest('build'))
+            .on('end',$.bs.reload);
+    });
+    var gulpif = require('gulp-if');
+    $.gulp.task('pug:build', function () {
+        return $.gulp.src('build/index.html')
+            .pipe($.gp.useref())
+            .pipe(gulpif('*.js', $.gp.uglify()))
+            .pipe(gulpif('*.css', $.gp.minifyCss()))
             .pipe($.gulp.dest('build'))
             .on('end',$.bs.reload);
     });
